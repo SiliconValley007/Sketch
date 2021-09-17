d2cc24f7f35ae31ffed1286b5d4554428013faf3
Debjeet Das <63968451+SiliconValley007@users.noreply.github.com>
2021-09-18 02:13:02 +0530

    Update Image_Sketch.py

--- Image_Sketch.py
+++ Image_Sketch.py
@@ -5,17 +5,23 @@ from tkinter import filedialog as fd
 from os import path
 from tkinter import messagebox
 
+#function to convert the image to a sketch
 def convert():
+    #get the file location from the tkinter entry box
     file_location = select_image_entry.get()
+    #read the file from its location using the open cv imread() function
     image = cv2.imread(file_location)
+    #converting the image to gray and inverting it ultimately resulting in an image that looks like a sketch
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     inverted_image = 255 - gray_image
     blurred = cv2.GaussianBlur(inverted_image, (21,21), 0)
     inverted_blurred = 255 - blurred
     pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+    #editing the file name so the original image does not get overwritten.
     new_file_location = file_location[:-4]+"_sketch"+file_location[-4:]
     cv2.imwrite(new_file_location, pencil_sketch)
-    messagebox.showinfo('Success', 'Image Successfully Converted')
+    #showing a message telling the user that the file was successfully converted into a sketch
+    messagebox.showinfo('Success', 'Image Successfully Converted. File stored at ' +  new_file_location)
 
 def selectfile():
     try:
